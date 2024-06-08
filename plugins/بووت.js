9f40e180e08f663fac58dc0257303016ca25a902
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-06-09 01:47:00 +0300

    Update بووت.js

--- plugins/بووت.js
+++ plugins/بووت.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `مرحبا معك بوت ناتسو اذا اردت شي اكتب (.مهام)`;
+    let message = `مرحبا معك ناتسو بوت اذا اردت شي اكتب (.اوامر)`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/82e0c0e8451ec05e854f3.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(bot|بوت|بووت|بوت)$/i;
+handler.customPrefix = /^(bot|بوت|بووت|ناتسو|bot|natsu)$/i;
 handler.command = new RegExp;
 
 export default handler;
