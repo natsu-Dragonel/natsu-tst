c2c78a73fcfab5a777f778f4b04ea97b737e57b5
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-06-11 19:36:41 +0300

    Update تشفير-فك التشفير.js

--- plugins/تشفير-فك التشفير.js
+++ plugins/تشفير-فك التشفير.js
@@ -33,7 +33,7 @@ const handler = async (m, { text, usedPrefix, command }) => {
     }
 }
 
-handler.command = ['تشفير', 'فك-شفرة'];
+handler.command = ['تشفير', 'فك-التشفير'];
 handler.tags = ['tools'];
 handler.rowner = true
 
