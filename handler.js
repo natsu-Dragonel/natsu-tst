c646139359052308340b6f5cf4a7e22c949c84b7
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-06-11 20:41:52 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1432,8 +1432,8 @@ if (chat.welcome) {
                   ppgp = await this.profilePictureUrl(id, 'image');
                 } catch (error) {
                   console.error(`حدث خطأ أثناء استرداد الصورة الشخصية: ${error}`);
-                  pp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
-                  ppgp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
+                  pp = 'https://telegra.ph/file/9742a853212c447ca62c7.png'; // Assign default image URL
+                  ppgp = 'https://telegra.ph/file/9742a853212c447ca62c7.png'; // Assign default image URL
                 } finally {
                   let text = (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user')
                     .replace('@group', await this.getName(id))
@@ -1441,7 +1441,7 @@ if (chat.welcome) {
                     .replace('@user', '@' + user.split('@')[0]);
           
                   let nthMember = groupMetadata.participants.length;
-                  let secondText = `اهلا ياحب, ${await this.getName(user)}, رقم ${nthMember}العضو`;
+                  let secondText = `اهلا ياحب نورت, ${await this.getName(user)}, Your number ${nthMember}`;
           
                   let welcomeApiUrl = `https://api.popcat.xyz/welcomecard?background=${encodeURIComponent(
                     'https://telegra.ph/file/919c9aa59b8dc5cae41a8.png'
@@ -1474,8 +1474,8 @@ if (chat.welcome) {
                   ppgp = await this.profilePictureUrl(id, 'image');
                 } catch (error) {
                   console.error(`حدث خطأ أثناء استرداد الصورة الشخصية: ${error}`);
-                  pp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
-                  ppgp = 'https://telegra.ph/file/d37b343ee8f981be6ffba.jpg'; // Assign default image URL
+                  pp = 'https://telegra.ph/file/9742a853212c447ca62c7.png'; // Assign default image URL
+                  ppgp = 'https://telegra.ph/file/9742a853212c447ca62c7.png'; // Assign default image URL
                 } finally {
                   let text = (chat.sBye || this.bye || conn.bye || 'اهلا, @user')
                     .replace('@user', '@' + user.split('@')[0]);
@@ -1484,7 +1484,7 @@ if (chat.welcome) {
                   let secondText = `وداعا, رقم ${nthMember}عضونا`;
           
                   let leaveApiUrl = `https://api.popcat.xyz/welcomecard?background=${encodeURIComponent(
-                    'https://telegra.ph/file/919c9aa59b8dc5cae41a8.png'
+                    'https://telegra.ph/file/9742a853212c447ca62c7.png'
                   )}&text1=${encodeURIComponent(
                     await this.getName(user)
                   )}&text2=الي+القاء&text3=عدد+الاعضاء:${encodeURIComponent(
