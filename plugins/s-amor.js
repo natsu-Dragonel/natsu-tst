d24e6a56b40b17e22194b93e44c4ed6f4fe71ab7
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-06-13 16:10:04 +0300

    Update s-amor.js

--- plugins/s-amor.js
+++ plugins/s-amor.js
@@ -1,14 +1,19 @@
 import { sticker } from '../lib/sticker.js';
 
 let handler = async (m, { conn }) => {
-    let nombre = '𝑁𝐴𝑇𝑆𝑈';
-    let nombre2 = '𝑁𝐴𝑇𝑆𝑈';
-    const s = [
-        'https://telegra.ph/file/784a05acc195cdb35ca29.jpg',
-    ];  
+    try {
+        let nombre = '𝑁𝐴𝑇𝑆𝑈';
+        let nombre2 = '𝑁𝐴𝑇𝑆𝑈';
+        const s = [
+            'https://telegra.ph/file/784a05acc195cdb35ca29.jpg',
+        ];  
 
-    let stiker = await sticker(null, s[Math.floor(Math.random() * s.length)], nombre, nombre2);
-    await conn.sendFile(m.chat, stiker, null, { asSticker: true, quoted: m });
+        let stiker = await sticker(null, s[Math.floor(Math.random() * s.length)], nombre, nombre2);
+        await conn.sendFile(m.chat, stiker, null, { asSticker: true, quoted: m });
+    } catch (e) {
+        console.error(e);
+        await conn.reply(m.chat, 'حدث خطأ أثناء محاولة إرسال الملصق.', m);
+    }
 };
 
 handler.customPrefix = /كسمك/i;
