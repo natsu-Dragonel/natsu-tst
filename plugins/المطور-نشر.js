3221807e9f31d81cfebaabf6ac942d973c0769e2
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-06-13 19:04:30 +0300

    Update المطور-نشر.js

--- plugins/المطور-نشر.js
+++ plugins/المطور-نشر.js
@@ -1,41 +1,53 @@
-/*---------------------------------------------------------------------------------------
-  🍀 • By https://github.com/ALBERTO9883
-  🍀 • ⚘Alberto Y Ashly⚘
------------------------------------------------------------------------------------------*/
+import { randomBytes } from 'crypto';
 
-import { randomBytes } from 'crypto'
-let link = /chat.whatsapp.com/
-let handler = async (m, { conn, text, groupMetadata }) => {
-if (m.isBaileys && m.fromMe)
-return !0
-if (!m.isGroup) return !1
-if (!text) throw '*_⚠ • ️Ingrese un -texto- para enviar un mensaje a todos los grupos._*'
-const linkThisGroup = `${link}`
-if (m.text.includes(linkThisGroup)) return conn.reply(m.chat, '❌ *_No puedes espamear enlaces a otros grupos._*', m)
-let time = global.db.data.users[m.sender].msgwait + 300000
-if (new Date - db.data.users[m.sender].msgwait < 300000) throw `*_⚠️ • Tienes que esperar ${msToTime(time - new Date())} para volver a enviar un mensaje._*`
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = await conn.getName(m.sender)
-let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
-let fakegif = { key: {participant: `0@s.whatsapp.net`, ...("201554824764-201554824764@g.us" ? { remoteJid: "201554824764-201554824764@g.us" } : {})},message: {"videoMessage": { "title": '𝔐𝔦𝔨𝔲 𝔅𝔒𝔗', "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': '𝑁𝐴𝑇𝑺𝑈 𝔅𝔒𝔗', 'jpegThumbnail': false }}}
-let teks = `*🌺 • الجروب:* ${groupMetadata.subject}\n*❄️ • من:* ${name}\n*👨‍💻 • رقم المطور:* wa.me/${who.split`@`[0]}\n*💌 • الرساله:* ${text}`
-for (let id of groups) {
-await conn.sendMessage(id, { text: teks }, { quoted: fakegif })
-global.db.data.users[m.sender].msgwait = new Date * 1
-}}
-handler.command = /^(اعلان)$/i
-handler.owner = true
-handler.group = true
-export default handler
+const link = /chat.whatsapp.com/;
+const handler = async (m, { conn, text, groupMetadata }) => {
+  if (m.isBaileys && m.fromMe) {
+    return !0;
+  }
+
+  if (!text) throw '*_⚠ • ️نسيت النص ي سيدي._*';
+
+  const linkThisGroup = `${link}`;
+  if (m.text.includes(linkThisGroup)) return conn.reply(m.chat, '❌ *مفيش جروبات في القائمه._*', m);
+
+  const time = global.db.data.users[m.sender].msgwait + 300000;
+  if (new Date - db.data.users[m.sender].msgwait < 300000) throw `*_⚠️ • Tienes que esperar ${msToTime(time - new Date())} para volver a enviar un mensaje._*`;
+
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  const name = await conn.getName(m.sender);
+
+  // Get all chats (groups and private)
+  const allChats = Object.entries(conn.chats).filter(([jid, chat]) => chat.isChats).map((v) => v[0]);
+
+  const fakegif = { key: { participant: `0@s.whatsapp.net`, ...('6289643739077-1613049930@g.us' ? { remoteJid: '6289643739077-1613049930@g.us' } : {}) }, message: { 'videoMessage': { 'title': '𝑁𝐴𝑇𝑆𝑈Bᴏᴛ - MD🍁⃨፝⃕✰', 'h': `Hmm`, 'seconds': '99999', 'gifPlayback': 'true', 'caption': '💵 𝑁𝐴𝑇𝑆𝑈 𝐁𝐎𝐓 👑', 'jpegThumbnail': false } } };
+
+  const teks = `*🎴 • من جروب:* ${groupMetadata.subject}\n*♦️ • المطور:* ${name}\n*🎟️ • الرقم:* wa.me/${who.split`@`[0]}\n*📧 • الرساله:* ${text}`;
+
+  for (const id of allChats) {
+    await conn.sendMessage(id, { text: teks }, { quoted: fakegif });
+  }
+
+  global.db.data.users[m.sender].msgwait = new Date * 1;
+};
+
+handler.command = /^(نشر|اذاعه)$/i;
+handler.owner = true;
+handler.group = true;
+
+export default handler;
+
 function msToTime(duration) {
-var milliseconds = parseInt((duration % 1000) / 100),
-seconds = Math.floor((duration / 1000) % 60),
-minutes = Math.floor((duration / (1000 * 60)) % 60),
-hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-hours = (hours < 10) ? "0" + hours : hours
-minutes = (minutes < 10) ? "0" + minutes : minutes
-seconds = (seconds < 10) ? "0" + seconds : seconds
-return minutes + " m " + seconds + " s " }
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+  const milliseconds = parseInt((duration % 1000) / 100);
+  let seconds = Math.floor((duration / 1000) % 60);
+  let minutes = Math.floor((duration / (1000 * 60)) % 60);
+  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+  return minutes + ' m ' + seconds + ' s ';
+}
+
+const more = String.fromCharCode(8206);
+const readMore = more.repeat(4001);
+const randomID = (length) => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length);
