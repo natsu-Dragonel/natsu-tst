708b9f2a2bc2bd83379241274c57db36e2b0ce61
natsu-Dragonel <160138068+natsu-Dragonel@users.noreply.github.com>
2024-06-13 15:40:03 +0300

    Update s-amor.js

--- plugins/s-amor.js
+++ plugins/s-amor.js
@@ -1,19 +1,18 @@
-import { sticker } from '../lib/sticker.js'
-let handler = async(m, { conn }) => {
-if (!db.data.chats[m.chat].stickers && m.isGroup) throw 0
+import { sticker } from '../lib/sticker.js';
 
-let nombre = '𝑁𝐴𝑇𝑺𝑈'
-let nombre2 = '𝑁𝐴𝑇𝑺𝑈' 
-const s = [
-'https://telegra.ph/file/784a05acc195cdb35ca29.jpg',
-];  
+let handler = async (m, { conn }) => {
+    let nombre = '𝑁𝐴𝑇𝑆𝑈';
+    let nombre2 = '𝑁𝐴𝑇𝑆𝑈';
+    const s = [
+        'https://telegra.ph/file/784a05acc195cdb35ca29.jpg',
+    ];  
 
-let stiker = await sticker(null, s[Math.floor(Math.random() * s.length)], nombre, nombre2)
-await delay(5 * 5000)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
-}
-handler.customPrefix = /كسمك/i 
-handler.command = new RegExp
-handler.exp = 50
-export default handler
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+    let stiker = await sticker(null, s[Math.floor(Math.random() * s.length)], nombre, nombre2);
+    await conn.sendFile(m.chat, stiker, null, { asSticker: true, quoted: m });
+};
+
+handler.customPrefix = /كسمك/i;
+handler.command = new RegExp;
+handler.exp = 50;
+
+export default handler;
